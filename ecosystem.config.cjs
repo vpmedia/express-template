879232096d9f5481e3379d3a3799c5de4c5fad60
Andras Csizmadia <andras@vpmedia.hu>
2024-02-12 18:34:26 +0100

    Bumped package versions

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      script: "./build/server.js",
-      watch: "./build",
-      log_type: "json",
-      node_args: "--experimental-modules",
+      script: './build/server.js',
+      watch: './build',
+      log_type: 'json',
+      node_args: '--experimental-modules',
       env_production: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
       },
       env_development: {
-        NODE_ENV: "development",
+        NODE_ENV: 'development',
       },
     },
   ],
